ecc08003658a50520b36fd6db617d45d62c53e6e
Wu <wqzhiep@gmail.com>
2023-04-29 11:38:18 +0000

    cppship 0.5.0
    Closes #14.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/cppship.rb
+++ Formula/cppship.rb
@@ -1,8 +1,8 @@
 class Cppship < Formula
   desc "Cargo-like c++ build tool based on conan2 and cmake"
   homepage "https://github.com/qqiangwu/cppship"
-  url "https://github.com/qqiangwu/cppship/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "3e214b42259e87e61e9ba47d80aac512aed4b80da8bf11fed2a830dd1b4b6fe2"
+  url "https://github.com/qqiangwu/cppship/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "0306e2ad247322e5d1bbbc84c3030839ee58e89e4128271e2950439aa5d237fc"
   license "BSL-1.0"
   head "https://github.com/qqiangwu/cppship.git", branch: "main"
 
