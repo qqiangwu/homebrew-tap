b36bef5ab516065349cedf0a1234972a930431bc
Wu <wqzhiep@gmail.com>
2023-12-29 16:30:09 +0000

    cppship 0.7.2

--- Formula/cppship.rb
+++ Formula/cppship.rb
@@ -1,8 +1,8 @@
 class Cppship < Formula
   desc "Cargo-like c++ build tool based on conan2 and cmake"
   homepage "https://github.com/qqiangwu/cppship"
-  url "https://github.com/qqiangwu/cppship/archive/refs/tags/v0.7.1.tar.gz"
-  sha256 "44a2a321be96bfbba4b70fe81924e0358664e91616f16c44a03f7115743e7e1a"
+  url "https://github.com/qqiangwu/cppship/archive/refs/tags/v0.7.2.tar.gz"
+  sha256 "ddf237d96b7fdd34ffa4b08f260380efb21aa20cf0d15642fdd8814c66e0ae23"
   license "BSL-1.0"
   head "https://github.com/qqiangwu/cppship.git", branch: "main"
 
