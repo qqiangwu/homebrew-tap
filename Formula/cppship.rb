0f27c5bd701f299e6e5ac91cb41a1446461535eb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-02 00:47:56 +0000

    cppship: update 0.8.0 bottle.

--- Formula/cppship.rb
+++ Formula/cppship.rb
@@ -8,10 +8,10 @@ class Cppship < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/qqiangwu/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "497ee90e441f7d9f8b1b8e95b827583710f8bdd1610a68a6f61785a1a2925378"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e1b0a5b2568ac841010297e0dc85d0eccb5ca0dce05c1eedfc149b21a479b11e"
-    sha256 cellar: :any_skip_relocation, ventura:       "ce1a7e135ad8ecec18b36d11c9b4021ce5f35d0b3689bed63960be5ae45fbb20"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "66d0c3b84d9c96dc00fa5ccfbaaca694821cc63fdadcf2072b6bdc015f9247de"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4eadf72c49e8c7b9d15e2c6c9745c4e99f9a6090a27c0575b20ab37a41d2350e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c8b113f7c52b7eeeb6f434761fbc138ba78170e797f6fe1db1f8c543bf7f47e1"
+    sha256 cellar: :any_skip_relocation, ventura:       "b8dbb2a0bbdb7fc8edbe212f4a2486b5eb0c5c662fb3fece3673c8dcb9eb496f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fdecacc1e404d520d3d85bf5914684a8d2c4e2ca5cc0881fc653119906415bc6"
   end
 
   depends_on "cmake"
